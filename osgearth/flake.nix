22a4db529a5b56a9f39adcabce80cd5ad8a168ed
John W. Terrell <john@alphapixel.com>
2026-08-09 16:49:35 -0700

    Mostly working

--- osgearth/flake.nix
+++ osgearth/flake.nix
@@ -1,35 +1,32 @@
 {
     description = "A flake for building OSGEarth using an OSG overlay that builds with GLCORE";
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     };
 
-    outputs = { nixpkgs, ... }:
+    outputs = { self, nixpkgs }: {
+      packages.aarch64-darwin.default =
         let
-            pkgs = nixpkgs.legacyPackages;
-
-            osgearth = {
-                pname = "osgearth";
-                version = "3.8.1";
-                src = pkgs.fetchFromGitHub {
-                    owner = "pelicanmapping";
-                    repo = "osgearth";
-                    rev = "osgearth-3.8.1";
-                    sha256 = "somehash";
-                };
+          pkgs = import nixpkgs { system = "aarch64-darwin"; };
+        in
+          pkgs.stdenv.mkDerivation {
+            pname = "osgearth";
+            version = "3.8.1";
+            src = pkgs.fetchFromGitHub {
+              owner = "pelicanmapping";
+              repo = "osgearth";
+              rev = "osgearth-3.8.1";
+              sha256 = "b543hdLllWvqUevsPL3c/2BSP53zS9ZXYm/BWLap+nU=";
             };
-        in {
-            "packages.${pkgs.system}" = {
-                default = nixpkgs.stdenv.mkDerivation {
-                    pname = "osgearth";
-                    version = "3.8.1";
-                    src = pkgs.fetchFromGitHub {
-                        owner = "pelicanapping";
-                        repo = "osgearth";
-                        rev = "osgearth-3.8.1";
-                        sha256 = "1233";
-                    };
-                };
-            };
-        };
+            nativeBuildInputs = with pkgs; [
+              cmake
+            ];
+            buildInputs = with pkgs; [
+              curl
+              gdal
+              geos
+              openscenegraph
+            ];
+          };
+    };
 }
