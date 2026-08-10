b91b366c09ac28c2fd78dd6db8ba3c0c3c9eeccb
John W. Terrell <john@alphapixel.com>
2026-08-09 17:36:19 -0700

    Made flake architecture agnostic.

--- osgearth/flake.nix
+++ osgearth/flake.nix
@@ -2,31 +2,36 @@
     description = "A flake for building OSGEarth using an OSG overlay that builds with GLCORE";
     inputs = {
       nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+      flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs }: {
-      packages.aarch64-darwin.default =
+    outputs = { self, nixpkgs, flake-utils }:
+      flake-utils.lib.eachDefaultSystem (system:
         let
-          pkgs = import nixpkgs { system = "aarch64-darwin"; };
+          pkgs = import nixpkgs { inherit system; };
         in
-          pkgs.stdenv.mkDerivation {
-            pname = "osgearth";
-            version = "3.8.1";
-            src = pkgs.fetchFromGitHub {
-              owner = "pelicanmapping";
-              repo = "osgearth";
-              rev = "osgearth-3.8.1";
-              sha256 = "b543hdLllWvqUevsPL3c/2BSP53zS9ZXYm/BWLap+nU=";
+          {
+            packages.default = pkgs.stdenv.mkDerivation {
+              pname = "osgearth";
+              version = "3.8.1";
+              src = pkgs.fetchFromGitHub {
+                owner = "pelicanmapping";
+                repo = "osgearth";
+                rev = "osgearth-3.8.1";
+                sha256 = "b543hdLllWvqUevsPL3c/2BSP53zS9ZXYm/BWLap+nU=";
+              };
+              nativeBuildInputs = with pkgs; [
+                cmake
+              ];
+              buildInputs = with pkgs; [
+                curl
+                gdal
+                geos
+                openscenegraph
+                libzip
+                sqlite
+              ];
             };
-            nativeBuildInputs = with pkgs; [
-              cmake
-            ];
-            buildInputs = with pkgs; [
-              curl
-              gdal
-              geos
-              openscenegraph
-            ];
-          };
-    };
+          }
+      );
 }
